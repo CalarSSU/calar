5960e4fd5116c50413a9d3418b963975a7772a9e
Michael Chernigin <michaelchernigin@gmail.com>
2022-10-05 15:06:56 +0400

    Refactor

--- scratch.py
+++ scratch.py
@@ -1,24 +1,26 @@
 import json
 import requests
 import icalendar
-#Нужно получать все токены с сайта
+import os
 
-def GetRequest (tokenDepartment, tokenEducation, tokenGroup):
-    return requests.get("https://scribabot.tk/api/v1.0/schedule/" 
-            + tokenEducation + "/" + tokenDepartment + "/" + str(tokenGroup))
+
+def GetRequest(tokenDepartment, tokenEducation, tokenGroup):
+    return requests.get("https://scribabot.tk/api/v1.0/schedule/" +
+                        tokenEducation + "/" + tokenDepartment + "/" +
+                        tokenGroup)
+
 
 def GetJson(tokenDepartment, tokenEducation, tokenGroup):
     getString = GetRequest(tokenDepartment, tokenEducation, tokenGroup)
     return json.loads(getString.text)
 
-def SaveFile(requestData, fileName , extention):
-        if extention == ".ics":
-            with open(file=fileName + extention, mode='wb') as file:
-                file.write(requestData.to_ical())
-        else:
-            with open(file=fileName + extention, mode='w') as file:
-                file.write(requestData.text) 
 
-
-    # scratch.SaveFile(scratch.GetRequest(Department, Education, Group),
-    #                 "timetable/"+ Department + '_' + str(Group), '.json')
+def SaveFile(requestData, filePath):
+    folder = filePath.rsplit('/', maxsplit=1)[0]
+    os.makedirs(folder, exist_ok=True)
+    if filePath.split('.')[-1] == 'ics':
+        with open(file=filePath, mode='wb') as file:
+            file.write(requestData.to_ical())
+    else:
+        with open(file=filePath, mode='w') as file:
+            file.write(requestData.text)
