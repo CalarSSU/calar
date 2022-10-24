f685a496cbc33881dc4ecaf7fff32d1780a03620
Michael Chernigin <59616661+mchernigin@users.noreply.github.com>
2022-10-24 15:50:21 +0400

    Subgroup in event title (#4)

--- convert.py
+++ convert.py
@@ -18,8 +18,7 @@ def json_to_ical(jsonData, subGroup):
     endDate = date(date.today().year, endMonth, endDay)
     startDate = date(date.today().year, startMonth, startDay)
     for event in jsonData['lessons']:
-        isCorrectSubGroup = (event['subGroup'] == ''
-                             or subGroup == ''
+        isCorrectSubGroup = (event['subGroup'] == '' or subGroup == ''
                              or event['subGroup'].split()[0] == subGroup
                              or event['subGroup'].split()[0].isalpha())
         if not isCorrectSubGroup:
@@ -41,30 +40,37 @@ def fill_event(iCal, curDate, event, startDate, endDate):
     isCorrectNom = (event['weekType'] == 'FULL'
                     or event['weekType'] == 'DENOM' and not isNom
                     or event['weekType'] == 'NOM' and isNom)
-    if isCorrectNom:
-        iEvent = icalendar.Event()
-        iEvent.add('name', event['name'])
-        iEvent.add('summary',
-                   event['name'] + ' ' + LESSON_TYPES[event["lessonType"]])
-        iTeacher = (event['teacher']['surname'] + " " +
-                    event['teacher']['name'] + " " +
-                    event['teacher']['patronymic'])
-        iEvent.add('description', iTeacher)
-        iEvent.add('location', event['place'])
-        diffDays = event['day']['dayNumber'] - weekday
-        eventDate = curDate + timedelta(days=diffDays)
-        if not startDate < eventDate < endDate:
-            return
 
-        iEvent.add(
-            'dtstart',
-            datetime(eventDate.year, eventDate.month, eventDate.day,
-                     event['lessonTime']['hourStart'],
-                     event['lessonTime']['minuteStart'], 0))
-        iEvent.add(
-            'dtend',
-            datetime(eventDate.year, eventDate.month, eventDate.day,
-                     event['lessonTime']['hourEnd'],
-                     event['lessonTime']['minuteEnd'], 0))
-        iCal.add_component(iEvent)
+    if not isCorrectNom:
+        return
 
+    subGroup = str(event['subGroup']).strip()
+    subGroupMention = ''
+    if subGroup != '' and not subGroup[0].isalpha():
+        subGroupMention = f'({subGroup}) '
+
+    iEvent = icalendar.Event()
+    iEvent.add('name', event['name'])
+    iEvent.add(
+        'summary', subGroupMention + event['name'] + ' ' +
+        LESSON_TYPES[event["lessonType"]])
+    iTeacher = (event['teacher']['surname'] + " " + event['teacher']['name'] +
+                " " + event['teacher']['patronymic'])
+    iEvent.add('description', iTeacher)
+    iEvent.add('location', event['place'])
+    diffDays = event['day']['dayNumber'] - weekday
+    eventDate = curDate + timedelta(days=diffDays)
+    if not startDate < eventDate < endDate:
+        return
+
+    iEvent.add(
+        'dtstart',
+        datetime(eventDate.year, eventDate.month, eventDate.day,
+                 event['lessonTime']['hourStart'],
+                 event['lessonTime']['minuteStart'], 0))
+    iEvent.add(
+        'dtend',
+        datetime(eventDate.year, eventDate.month, eventDate.day,
+                 event['lessonTime']['hourEnd'],
+                 event['lessonTime']['minuteEnd'], 0))
+    iCal.add_component(iEvent)
